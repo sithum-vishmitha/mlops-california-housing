3be101cb3a426021b04c67b4db9567234c546047
Sithum Vishmitha <sithum20034@gmaiil.com>
2026-07-13 13:38:34 +1000

    Change model

--- src/train.py
+++ src/train.py
@@ -4,12 +4,16 @@ import joblib
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
+
+def build_model():
+    return RandomForestRegressor(n_estimators=100 , random_state=42)
 def main() :
     train_df = pd.read_csv('../data/processed/train.csv')
     x_train = train_df.drop("MedHouseVal" , axis=1)   # axis =  1 remove column
     y_train = train_df['MedHouseVal']
-    model =  LinearRegression()
+    model =  build_model()
     model.fit(x_train , y_train)
     print("Model trained successfully.")
     Path("../models").mkdir(exist_ok=True)
