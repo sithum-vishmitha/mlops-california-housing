be43f26105c4bd3546fb12fde493c1190630ee05
Sithum Vishmitha <sithum20034@gmaiil.com>
2026-07-13 17:51:28 +1000

    Commit the chages of the path

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,10 +5,10 @@ from sklearn.model_selection import train_test_split
 
 
 def main():
-    input_file  = Path("../data/raw/california_housing.csv")
+    input_file  = Path("data/raw/california_housing.csv")
     df = pd.read_csv(input_file)
     train_df , test_df=  train_test_split(df , test_size = 0.2, random_state = 42)
-    processed_dir = Path("../data/processed")
+    processed_dir = Path("data/processed")
     processed_dir.mkdir(parents=True, exist_ok=True)
     train_df.to_csv(processed_dir / "train.csv", index = False)
     test_df.to_csv(processed_dir / "test.csv", index = False)
