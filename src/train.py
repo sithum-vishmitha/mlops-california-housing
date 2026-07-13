abe439365b913ba654fb46a0194f8d169cb7e35a
Sithum Vishmitha <sithum20034@gmaiil.com>
2026-07-13 17:59:16 +1000

    Add DVC pipeline

--- src/train.py
+++ src/train.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def build_model():
-    return RandomForestRegressor(n_estimators=100 , random_state=42)
+    return RandomForestRegressor(n_estimators=100 , random_state=10)
 def main() :
 
     train_df = pd.read_csv('data/processed/train.csv')
